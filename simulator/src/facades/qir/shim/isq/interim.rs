5b668c05d026d4e69f16c08c58110e2c8f50ba07
gjz010 <gjz010944@gmail.com>
2022-02-16 18:05:18 +0800

    runnable bell example.

--- simulator/src/facades/qir/shim/isq/interim.rs
+++ simulator/src/facades/qir/shim/isq/interim.rs
@@ -10,3 +10,13 @@ pub extern "C" fn __isq__qir__shim__qis__gphase(x0: f64)->() {
     use core::mem::transmute as t;
     unsafe { t(isq_qir_shim_qis_gphase(t::<_, f64>(x0)))}
 }
+#[no_mangle]
+pub extern "C" fn __isq__qir__shim__qis__cnot(x0: *mut i8, x1: *mut i8)->() {
+    use core::mem::transmute as t;
+    unsafe { t(isq_qir_shim_qis_cnot(t::<_, K<QIRQubit>>(x0), t::<_, K<QIRQubit>>(x1)))}
+}
+#[no_mangle]
+pub extern "C" fn __isq__qir__shim__qis__measure(x0: *mut i8)->*mut i8 {
+    use core::mem::transmute as t;
+    unsafe { t(isq_qir_shim_qis_measure(t::<_, K<QIRQubit>>(x0)))}
+}
