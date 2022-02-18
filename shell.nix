69a0464fff05e8228d76f65cedee770eca6b135b
gjz010 <gjz010944@gmail.com>
2022-02-18 13:39:58 +0800

    pin nixpkgs-mozilla version

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/f233fdc4ff6ba2ffeb1e3e3cd6d63bb1297d6996.tar.gz);
   pkgs = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/c6019d8efb5.tar.gz) { overlays = [ moz_overlay ]; };
   lib = pkgs.lib;
   rustChannel = (pkgs.rustChannelOf { rustToolchain = ./rust-toolchain; });
