8959e8e3b20b861a150460d963468cd9fca33303
Jintaoyu <yjt98765@gmail.com>
2022-08-17 17:43:43 +0800

    Add quantum algorithms to examples
    Also update package.yaml.

--- frontend/cabal.nix
+++ frontend/cabal.nix
@@ -1,19 +1,32 @@
 { mkDerivation, aeson, alex, array, base, bytestring, containers
-, deepseq, directory, extra, happy, hpack, lens, lib, multimap
-, math-functions, mtl, parsec, pretty-simple, split, text, filepath
+, deepseq, directory, extra, filepath, happy, hpack, hspec, lens
+, lib, math-functions, mtl, multimap, parsec, pretty-simple, split
+, text
 }:
 mkDerivation {
   pname = "isqc-frontend";
   version = "0.1.0.0";
   src = ./.;
-  isLibrary = false;
+  isLibrary = true;
   isExecutable = true;
-  libraryToolDepends = [ hpack ];
+  libraryHaskellDepends = [
+    aeson array base bytestring containers deepseq directory extra
+    filepath lens math-functions mtl multimap parsec pretty-simple
+    split text
+  ];
+  libraryToolDepends = [ alex happy hpack ];
   executableHaskellDepends = [
-    aeson array base bytestring containers deepseq directory extra lens
-    math-functions mtl parsec pretty-simple split text filepath multimap
+    aeson array base bytestring containers deepseq directory extra
+    filepath lens math-functions mtl multimap parsec pretty-simple
+    split text
   ];
   executableToolDepends = [ alex happy ];
+  testHaskellDepends = [
+    aeson array base bytestring containers deepseq directory extra
+    filepath hspec lens math-functions mtl multimap parsec
+    pretty-simple split text
+  ];
+  testToolDepends = [ alex happy ];
   prePatch = "hpack";
   homepage = "https://github.com/gjz010/isqv2-frontend#readme";
   license = lib.licenses.bsd3;
