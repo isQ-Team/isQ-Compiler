82df4b410059ecc9c5c8086a6752e0c1b74b5f7a
gjz010 <gjz010944@gmail.com>
2023-01-27 15:43:56 +0800

    nixpkgs fmt

--- mlir/flake.nix
+++ mlir/flake.nix
@@ -5,18 +5,18 @@
     gitignore.url = "github:hercules-ci/gitignore.nix";
     mlir.url = "path:../base/vendor/mlir";
   };
-  outputs = { self, isqc-base, gitignore, mlir, ...}: 
-  isqc-base.lib.isqc-components-flake {
-    inherit self;
-    overlay = isqc-base.lib.isqc-override (pkgs: final: prev: {
-        isq-opt =  (final.callPackage ./default.nix {gitignoreSource = gitignore.lib.gitignoreSource;});
-    });
-    components = ["isq-opt"];
-    defaultComponent = "isq-opt";
-    depComponentOverlays = [mlir.overlays.default];
-    shell = {pkgs}: pkgs.mkShell.override {stdenv = pkgs.llvmPackages.stdenv;} {
-      inputsFrom = [pkgs.isqc.isq-opt];
-      nativeBuildInputs = [pkgs.clang-tools];
+  outputs = { self, isqc-base, gitignore, mlir, ... }:
+    isqc-base.lib.isqc-components-flake {
+      inherit self;
+      overlay = isqc-base.lib.isqc-override (pkgs: final: prev: {
+        isq-opt = (final.callPackage ./default.nix { gitignoreSource = gitignore.lib.gitignoreSource; });
+      });
+      components = [ "isq-opt" ];
+      defaultComponent = "isq-opt";
+      depComponentOverlays = [ mlir.overlays.default ];
+      shell = { pkgs }: pkgs.mkShell.override { stdenv = pkgs.llvmPackages.stdenv; } {
+        inputsFrom = [ pkgs.isqc.isq-opt ];
+        nativeBuildInputs = [ pkgs.clang-tools ];
+      };
     };
-  };
 }
--- simulator/plugins/cuda-plugin/default.nix
+++ simulator/plugins/cuda-plugin/default.nix
@@ -1,14 +1,14 @@
-{stdenv, cudaPackages_11_5, linuxPackages, addOpenGLRunpath, makeWrapper}:
-stdenv.mkDerivation{
-    name = "isq-simulator-plugin-cuda";
-    buildInputs = [cudaPackages_11_5.cudatoolkit linuxPackages.nvidia_x11 addOpenGLRunpath makeWrapper];
-    shellHook = ''
-       export CUDA_PATH=${cudaPackages_11_5.cudatoolkit}
-       export EXTRA_LDFLAGS="-L/lib -L${linuxPackages.nvidia_x11}/lib"
-       export EXTRA_CCFLAGS="-I/usr/include"
-    '';
-    src = ./.;
-    installPhase = ''
+{ stdenv, cudaPackages_11_5, linuxPackages, addOpenGLRunpath, makeWrapper }:
+stdenv.mkDerivation {
+  name = "isq-simulator-plugin-cuda";
+  buildInputs = [ cudaPackages_11_5.cudatoolkit linuxPackages.nvidia_x11 addOpenGLRunpath makeWrapper ];
+  shellHook = ''
+    export CUDA_PATH=${cudaPackages_11_5.cudatoolkit}
+    export EXTRA_LDFLAGS="-L/lib -L${linuxPackages.nvidia_x11}/lib"
+    export EXTRA_CCFLAGS="-I/usr/include"
+  '';
+  src = ./.;
+  installPhase = ''
     runHook preInstall
     mkdir -p $out/bin
     mkdir -p $out/lib
@@ -18,9 +18,9 @@ stdenv.mkDerivation{
     cp qsim_kernel.h $out/include/qsim_kernel.h
     moveToOutput bin "''${!outputBin}"
     runHook postInstall
-    '';
-    postFixup = ''
-      addOpenGLRunpath $out/lib/libqsim_kernel.so
-    '';
-    outputs = [ "out" "dev" "bin"  ];
+  '';
+  postFixup = ''
+    addOpenGLRunpath $out/lib/libqsim_kernel.so
+  '';
+  outputs = [ "out" "dev" "bin" ];
 }
