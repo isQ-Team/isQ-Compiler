f13597ba4f37400d34c638e0daa6ae576e612938
gjz010 <gjz010944@gmail.com>
2023-01-29 09:49:59 +0800

    trivial template

---
+++ templates/trivial/flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    isq-compiler.url = "github:arclight-quantum/isQ-Compiler";
+  };
+
+  outputs = { self, nixpkgs, isq-compiler, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [ isq-compiler.legacyPackages.${system}.isqc ];
+        };
+      }
+    );
+}
