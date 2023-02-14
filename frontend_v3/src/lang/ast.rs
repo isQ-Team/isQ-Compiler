b8da36ec70ec30df4ec82e607afb02426e69f294
gjz010 <gjz010944@gmail.com>
2023-02-14 22:04:35 +0800

    wip

--- frontend_v3/src/lang/ast.rs
+++ frontend_v3/src/lang/ast.rs
@@ -11,7 +11,7 @@ pub enum BinaryOp{
 }
 #[derive(Copy, Clone, Debug, PartialOrd, Ord, PartialEq, Eq)]
 pub enum UnaryOp{
-    Neg, Pos, Not
+    Neg, Pos, Not, Borrow, Deref
 }
 
 #[derive(Debug, Clone)]
@@ -37,7 +37,7 @@ pub enum ExprNode<E>{
         lhs: Expr<E>,
         rhs: Expr<E>
     },
-    Brace(Expr<E>),
+    Paren(Expr<E>),
     Unary{
         op: UnaryOp,
         arg: Expr<E>
@@ -59,45 +59,82 @@ pub enum ExprNode<E>{
         step: Option<Expr<E>>,
     },
     List(Vec<Expr<E>>),
+    Unit
 }
 
 #[derive(Debug, Clone)]
 pub struct Expr<E>(pub Box<ExprNode<E>>, pub E);
 
 
+/** These are builtin data types supported by isQ. */
+#[derive(Debug, Clone)]
+pub enum VarLexicalTyType<T>{
+    /** Default integer type.*/
+    Int, 
+    /** Reference-alike qubit type. */
+    Qbit, 
+    /** Default fp64 type. */
+    Double, 
+    /** Boolean type. */
+    Boolean, 
+    /** Reserved for qualified type. */
+    Named(Qualified<T>), 
+    /** Owned value reference that can automatically degenerate into value.  */
+    Owned(VarLexicalTy<T>), 
+    /** Reference type that does not own underlying value. */
+    Ref(VarLexicalTy<T>), 
+    /** A contiguous array of data with determined length. */
+    Array(VarLexicalTy<T>, usize), 
+    /** Slice type that does not own underlying array.
+     * Contains length and underlying pointer information.
+     */
+    Slice(VarLexicalTy<T>), 
+    /** Range (lo, hi) that can be used in loops as well as slicing contiguous array. */
+    Range, 
+    /** Range (lo, hi, step) that can be used in loops. */
+    Range3 ,
+    /** Unit type */
+    Unit
+}
+
+#[derive(Debug, Clone)]
+pub struct VarLexicalTy<T>(pub Box<VarLexicalTyType<T>>, pub T);
+
 
 #[derive(Debug, Clone)]
 pub struct VarDef<T>{
     pub var: Ident<T>,
-    pub ty: Qualified<T>,
+    pub ty: Option<VarLexicalTy<T>>,
 }
 
 #[derive(Debug, Clone)]
 pub enum DerivingClauseType{
-    Gate
+    Gate, Oracle
 }
 #[derive(Debug, Clone)]
 pub struct DerivingClause<T>(DerivingClauseType, pub T);
 
+#[derive(Debug, Clone)]
+pub struct ASTBlock<E, T>(pub Vec<AST<E, T>>, pub T);
 
 
 #[derive(Debug, Clone)]
 pub enum ASTNode<E, T>{
-    Block(AST<E, T>),
+    Block(ASTBlock<E, T>),
     Expr(Expr<E>),
     If{
         condition: Expr<E>,
-        then_block: AST<E, T>,
-        else_block: AST<E, T>
+        then_block: ASTBlock<E, T>,
+        else_block: Option<ASTBlock<E, T>>
     },
     For{
         var: Ident<T>,
         range: Expr<E>,
-        body: Vec<AST<E, T>>,
+        body: ASTBlock<E, T>,
     },
     While{
-        cond: Expr<E>,
-        body: Vec<AST<E, T>>,
+        condition: Expr<E>,
+        body: ASTBlock<E, T>,
     },
     Defvar{
         definitions: Vec<(VarDef<T>, Option<Expr<E>>)>
@@ -130,3 +167,4 @@ pub struct AST<E, T>(pub Box<ASTNode<E, T>>, pub T);
 // Lexical expr with span info attached.
 pub type LExpr = Expr<Span>;
 pub type LAST = AST<Span, Span>;
+pub type LASTBlock = ASTBlock<Span, Span>;
