748d75a4c6d0b1d53149e2620ba2ba541cb4857d
gjz010 <gjz010944@gmail.com>
2023-03-20 19:53:27 +0800

    wip: extra dialect

---
+++ mlir/lib/extra/ExtraDialect.cpp
@@ -0,0 +1,6 @@
+#include <isq/dialects/Extra.h>
+#include <isq/tblgen/ISQExtraAttrs.cpp.inc>
+#include <isq/tblgen/ISQExtraDialect.cpp.inc>
+#define GET_OP_CLASSES
+#include <isq/tblgen/ISQExtraOPs.cpp.inc>
+#include <isq/tblgen/ISQExtraTypes.cpp.inc>
