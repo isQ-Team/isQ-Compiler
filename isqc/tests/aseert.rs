4949938adf432de593a4166b5058ed698ecd9a23
Jintao Yu <yjt98765@gmail.com>
2023-03-30 15:36:30 +0800

    Commit merge from 8a61 to 76062dbe

--- isqc/tests/aseert.rs
+++ isqc/tests/aseert.rs
@@ -9,11 +9,21 @@ use test_case::test_case;
 #[test_case("array_negative", "Error: index out of bound")]
 #[test_case("array_out_of_bound", "Error: index out of bound")]
 #[test_case("divide_0", "Error: divide 0")]
-fn test_assert(name: &str, res: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn test_assert_stdout(name: &str, res: &str) -> Result<(), Box<dyn std::error::Error>> {
     let source_file = name.to_string() + ".isq";
     let folder = Path::new("tests").join("input").join("assert");
     let mut cmd = Command::cargo_bin("isqc")?;
     cmd.arg("run").arg("--debug").arg(folder.join(source_file).to_str().to_owned().unwrap());
     cmd.assert().success().stdout(predicate::str::contains(res));
+    Ok(())
+}
+
+#[test_case("length_mismatch", "op requires the same shape")]
+fn test_assert_stderr(name: &str, res: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let source_file = name.to_string() + ".isq";
+    let folder = Path::new("tests").join("input").join("assert");
+    let mut cmd = Command::cargo_bin("isqc")?;
+    cmd.arg("run").arg("--debug").arg(folder.join(source_file).to_str().to_owned().unwrap());
+    cmd.assert().success().stderr(predicate::str::contains(res));
     Ok(())
 }
