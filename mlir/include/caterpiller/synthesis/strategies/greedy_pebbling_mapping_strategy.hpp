a3ed15f8b72bf2d9a35bd931903d3ed29f2cc3a1
root <root@LAPTOP-JC95M2KP>
2023-04-04 15:07:08 +0800

    optimized greedy pebbling strategy

--- mlir/include/caterpiller/synthesis/strategies/greedy_pebbling_mapping_strategy.hpp
+++ mlir/include/caterpiller/synthesis/strategies/greedy_pebbling_mapping_strategy.hpp
@@ -23,6 +23,9 @@ public:
         static_assert( mt::is_network_type_v<LogicNetwork>, "LogicNetwork is not a network type" );
         static_assert( mt::has_foreach_po_v<LogicNetwork>, "LogicNetwork does not implement the foreach_po method" );
         static_assert( mt::has_foreach_fanin_v<LogicNetwork>, "LogicNetwork does not implement the foreach_fanin method" );
+        static_assert( mt::has_foreach_node_v<LogicNetwork>, "LogicNetwork does not implement the foreach_node method" );
+        static_assert( mt::has_is_constant_v<LogicNetwork>, "LogicNetwork does not implement the is_constant method" );
+        static_assert( mt::has_is_pi_v<LogicNetwork>, "LogicNetwork does not implement the is_pi method" );
         static_assert( mt::has_set_visited_v<LogicNetwork>, "LogicNetwork does not implement the set_visited method" );
         static_assert( mt::has_visited_v<LogicNetwork>, "LogicNetwork does not implement the visited method" );
         static_assert( mt::has_clear_visited_v<LogicNetwork>, "LogicNetwork does not implement the clear_visited method" );
@@ -33,16 +36,37 @@ public:
     bool compute_steps( LogicNetwork const& ntk ) override
     {
         clear_containers();
-        construct_connectivity(ntk);
+
+        // topo_view rules out the nodes unreachable from pos
+        mockturtle::topo_view view{ntk};
+        _ntk = ntk;
+        _view = view;
+        construct_connectivity();
+
+        // set start, target and all nodes
+        std::set<node_t> start;
+        std::set<node_t> target;
+        std::set<node_t> nodes;
+        view.foreach_node( [&] ( auto node ) {
+            if (view.is_constant(node) || view.is_pi(node)) start.insert(node);
+            nodes.insert(node);
+        } );
+        ntk.foreach_po( [&] ( auto sig, auto po_index ) {
+            auto po = ntk.get_node(sig);
+            target.insert(po);
+            pos.insert(po);
+        } );
+        greedy_pebble(nodes, start, target);
+        /*
         compute_connected_components(ntk);
         for (auto connected_component : connected_components) {
             greedy_pebble(connected_component, ntk);
-        }
+        }*/
         // TODO
         return true;
     }
 
-    void print_connected_component(std::ostream &os) {
+    void print_connected_component(std::ostream &os, std::vector<std::set<node_t>> connected_components) {
         os << "******connected component******" << std::endl;
         int i = 0;
         for (auto component : connected_components) {
@@ -59,40 +83,40 @@ public:
     }
 
 private:
-    std::set<node_t> RN(std::set<node_t> a, LogicNetwork const& ntk) {
+    std::set<node_t> RN(std::set<node_t> a, std::set<node_t>& nodes) {
         std::set<node_t> result;
-        for (auto node : a) {
-            for (auto fanout : node_to_fanout[node]) {
-                if (a.find(fanout) != a.end()) continue;
-                if (result.find(fanout) != result.end()) continue;
-                bool all_fanin_in = true;
-                for (auto fanin : node_to_fanin[fanout]) {
-                    if (a.find(fanin) == a.end()) all_fanin_in = false;
-                }
-                if (all_fanin_in) result.insert(fanout);
+        for (auto node : fanout_set(a, nodes)) {
+            if (!nodes.count(node)) continue;
+            bool all_fanin_in = true;
+            for (auto fanin : node_to_fanin[node]) {
+                if (!a.count(fanin)) all_fanin_in = false;
             }
+            if (all_fanin_in) result.insert(node);
         }
-        ntk.foreach_pi( [&] ( auto pi ) {
-            for (auto fanout : node_to_fanout[pi]) {
-                if (a.find(fanout) == a.end()) result.insert(fanout);
-            }
-        } );
         return result;
     }
 
-    std::set<node_t> RP(std::set<node_t> a, LogicNetwork const& ntk) {
+    std::set<node_t> RP(std::set<node_t> a, std::set<node_t>& nodes) {
         std::set<node_t> result;
         for (auto node : a) {
             for (auto fanin : node_to_fanin[node]) {
-                if (ntk.is_constant(fanin) || ntk.is_pi(fanin)) continue;
-                if (a.find(fanin) != a.end()) continue;
+                if (!nodes.count(fanin)) continue;
+                if (a.count(fanin)) continue;
+                if (_ntk.is_constant(fanin) || _ntk.is_pi(fanin)) continue;
                 result.insert(fanin);
             }
         }
-        ntk.foreach_po( [&] ( auto sig, auto po_index ) {
-            auto po = ntk.get_node(sig);
-            if (a.find(po) == a.end()) result.insert(po);
-        } );
+        return result;
+    }
+
+    std::set<node_t> fanout_set(std::set<node_t> a, std::set<node_t>& nodes) {
+        std::set<node_t> result;
+        for (auto node : a) {
+            for (auto fanout : node_to_fanout[node]) {
+                if (a.count(fanout)) continue;
+                if (nodes.count(fanout)) result.insert(fanout);
+            }
+        }
         return result;
     }
 
@@ -108,124 +132,183 @@ private:
         return result;
     }
 
-    void compute_steps_set(std::set<node_t>& s, mockturtle::topo_view<LogicNetwork>& view, bool reverse = false) {
-        auto it = this->steps().end();
-        if (reverse) {
-            view.foreach_node( [&] ( auto node ) {
-                if (view.is_constant(node) || view.is_pi(node)) return;
-                if (s.find(node) == s.end()) return;
-                if (pos.count(node)) return;
-                it = this->steps().insert(it, {node, uncompute_action{}});
-            } );
-        } else {
-            view.foreach_node( [&] ( auto node ) {
-                if (view.is_constant(node) || view.is_pi(node)) return;
-                if (s.find(node) == s.end()) return;
-                it = this->steps().insert(it, {node, compute_action{}});
-                it++;
-            } );
-        }
+    std::set<node_t> set_union(std::set<node_t> a, std::set<node_t> b) {
+        std::set<node_t> result;
+        std::set_union(a.begin(), a.end(), b.begin(), b.end(), std::inserter(result, result.begin()));
+        return result;
     }
 
-    void greedy_pebble(std::set<node_t> nodes, LogicNetwork const& ntk, int beta = 0) {
-        mockturtle::topo_view view{ntk};
-        if (nodes.size() <= 4) {
-            compute_steps_set(nodes, view);
-            compute_steps_set(nodes, view, true);
+    // target is a subset of s
+    void compute_set_steps(std::set<node_t>& s, std::set<node_t>& target, bool reverse = false) {
+        auto it = this->steps().end();
+        _view.foreach_node( [&] ( auto node ) {
+            if (_view.is_constant(node) || _view.is_pi(node)) return;
+            if (!s.count(node)) return;
+            if (target.count(node)) {
+                if (reverse) {
+                    if (pos.count(node)) return;
+                    it = this->steps().insert(it, {node, uncompute_action{}});
+                    it++;
+                } else {
+                    it = this->steps().insert(it, {node, compute_action{}});
+                    it++;
+                }
+            } else {
+                it = this->steps().insert(it, {node, compute_action{}});
+                it++;
+                it = this->steps().insert(it, {node, uncompute_action{}});
+            }
+        } );
+        /*
+        std::cout << "print steps: " << std::endl;
+        auto it_t = this->steps().begin();
+        while (it_t != this->steps().end()) {
+            std::cout << it_t->first << std::endl;
+            it_t++;
+        }
+        std::cout << "print steps ends here" << std::endl;*/
+    }
+
+    void greedy_pebble(std::set<node_t> nodes, std::set<node_t> start, std::set<node_t> target, bool reverse = false) {
+        std::vector<std::set<node_t>> connected_components;
+        compute_connected_components(nodes, connected_components);
+
+        for (auto connected_component : connected_components) {
+            divide_and_conquer(connected_component, start, intersection(connected_component, target), reverse);
+        }
+    }
+    
+    // target <= nodes, intersection(nodes, start) = empty
+    void divide_and_conquer(std::set<node_t> nodes, std::set<node_t> start, std::set<node_t> target, bool reverse = false) {
+        if (difference(nodes, target).size() <= 4) {
+            compute_set_steps(nodes, target, reverse);
             return;
         }
-        if (!beta) beta = nodes.size() * 2 / 3;
+        int beta = nodes.size() * 2 / 3;
 
         std::set<node_t> A;
         std::set<node_t> B = difference(nodes, A);  // V - A
-        std::set<node_t> C = intersection(RN(A, ntk), nodes);
+        std::set<node_t> C = RN(start, nodes);
         int target_size = nodes.size() - beta;
         while (A.size() <= target_size) {
             int rn_max_size = 0, n_max_size = 0;
+            int rn_now_size = C.size();
             node_t next;
             for (auto i : C) {
+                int rn_size_i = rn_now_size - 1, n_size_i = 0;
+                for (auto fanout : node_to_fanout[i]) {
+                    if (!nodes.count(fanout)) continue;
+                    bool all_fanin_in_A = true;
+                    for (auto fanin_of_fanout : node_to_fanin[fanout]) {
+                        if (i == fanin_of_fanout) continue;
+                        if (A.count(fanin_of_fanout) || start.count(fanin_of_fanout)) continue;
+                        all_fanin_in_A = false;
+                    }
+                    if (all_fanin_in_A) rn_size_i++;
+                    if (B.count(fanout)) n_size_i++;
+                }
+
+                if (rn_size_i > rn_max_size || (rn_size_i == rn_max_size && n_size_i > n_max_size)) {
+                    next = i;
+                    rn_max_size = rn_size_i;
+                    n_max_size = n_size_i;
+                }
+                /*
                 std::set<node_t> i_A;  // {i} + A
                 std::copy(A.begin(), A.end(), std::inserter(i_A, i_A.begin()));
                 i_A.insert(i);
-                std::set<node_t> rn_i_A = intersection(RN(i_A, ntk), nodes);
-                std::set<node_t> n_i_B = intersection(B, node_to_fanout[i]);/*
-                std::cout << i << std::endl;
-                std::cout << "rn_i_A.size: " << rn_i_A.size() << std::endl;
-                std::cout << "rn_max_size: " << rn_max_size << std::endl;
-                std::cout << "n_i_B.size: " << n_i_B.size() << std::endl;
-                std::cout << "n_max_size: " << n_max_size << std::endl;*/
+                std::set<node_t> rn_i_A = RN(set_union(i_A, start), nodes);
+                std::set<node_t> n_i_B = intersection(B, node_to_fanout[i]);
+                
                 if (rn_i_A.size() > rn_max_size || (rn_i_A.size() == rn_max_size && n_i_B.size() > n_max_size)) {
                     next = i;
                     rn_max_size = rn_i_A.size();
                     n_max_size = n_i_B.size();
-                }
+                }*/
+                /*
+                std::cout << i << std::endl;
+                std::cout << "rn_i_A.size: " << rn_i_A.size() << std::endl;
+                std::cout << "rn_max_size: " << rn_max_size << std::endl;
+                std::cout << "n_i_B.size: " << n_i_B.size() << std::endl;
+                std::cout << "n_max_size: " << n_max_size << std::endl;
+                */
+                
             }
             A.insert(next);
-            B = difference(nodes, A);  // V - A
-            C = intersection(RN(A, ntk), nodes);/*
+            B.erase(next);  // V - A
+            // update C
+            
+            C.erase(next);
+            for (auto fanout : node_to_fanout[next]) {
+                if (!nodes.count(fanout)) continue;
+                bool all_fanin_in_A = true;
+                for (auto fanin_of_fanout : node_to_fanin[fanout]) {
+                    if (next == fanin_of_fanout) continue;
+                    if (A.count(fanin_of_fanout) || start.count(fanin_of_fanout)) continue;
+                    all_fanin_in_A = false;
+                }
+                if (all_fanin_in_A) C.insert(fanout);
+            }
+            //C = RN(set_union(A, start), nodes);
+            /*
             print_to(std::cout, A, "A");
             print_to(std::cout, B, "B");
-            print_to(std::cout, C, "C");*/
+            print_to(std::cout, C, "C");
+            */
         }
-        std::set<node_t> S = intersection(RP(difference(B, C), ntk), nodes);
-        ntk.foreach_pi( [&] ( auto pi ) {
-            for (auto fanout : node_to_fanout[pi]) {
-                S.erase(fanout);
-            }
-        } );
+        std::set<node_t> fanout_of_start = RN(start, nodes);
+        C = difference(C, fanout_of_start);
+        //C = RN(A, nodes);
+        std::set<node_t> S = RP(difference(B, C), nodes);
         A = difference(A, S);
-        B = difference(B, S);/*
+        B = difference(B, S);
+        /*
+        std::cout << "****** computation of s complete!!! ******" << std::endl;
         print_to(std::cout, A, "A");
         print_to(std::cout, B, "B");
-        print_to(std::cout, S, "S");*/
-        compute_steps_set(A, view);
-        compute_steps_set(S, view);
-        compute_steps_set(A, view, true);
-        compute_steps_set(B, view);
-        compute_steps_set(B, view, true);
-        compute_steps_set(A, view);
-        compute_steps_set(S, view, true);
-        compute_steps_set(A, view, true);
+        print_to(std::cout, S, "S");
+        */
+        greedy_pebble(set_union(A, S), start, set_union(S, intersection(target, A)));
+        greedy_pebble(B, set_union(S, start), intersection(target, B), reverse);
+        greedy_pebble(set_union(A, S), start, set_union(S, intersection(target, A)), true);
     }
 
-    void compute_connected_components(LogicNetwork const& ntk) 
+    void compute_connected_components(std::set<node_t>& nodes, std::vector<std::set<node_t>>& connected_components) 
     {
-        ntk.clear_visited();
+        _ntk.clear_visited();
         uint32_t visited_flag = 0;
-        ntk.foreach_po( [&] ( auto sig, auto po_index ) {
+        for (auto node : nodes) {
             std::set<node_t> connected_component;
             std::stack<node_t> sta;
-            auto po = ntk.get_node(sig);
-            pos.insert(po);
-            sta.push(po);
+            sta.push(node);
             visited_flag++;
             while (!sta.empty()) {
                 auto node = sta.top();
                 sta.pop();
-                if (ntk.is_constant(node) || ntk.is_pi(node)) continue;
-                if (ntk.visited(node)) continue;
+                if (!nodes.count(node)) continue;
+                if (_ntk.is_constant(node) || _ntk.is_pi(node)) continue;
+                if (_ntk.visited(node)) continue;
                 connected_component.insert(node);
-                ntk.set_visited(node, visited_flag);
+                _ntk.set_visited(node, visited_flag);
                 for (auto fanin : node_to_fanin[node]) {
-                    if (!ntk.visited(fanin)) sta.push(fanin);
+                    if (!_ntk.visited(fanin)) sta.push(fanin);
                 }
                 for (auto fanout : node_to_fanout[node]) {
-                    if (!ntk.visited(fanout)) sta.push(fanout);
+                    if (!_ntk.visited(fanout)) sta.push(fanout);
                 }
             }
             if (connected_component.empty()) visited_flag--;
             else connected_components.push_back(connected_component);
-        } );
-        ntk.clear_visited();
+        }
+        _ntk.clear_visited();
         std::sort(connected_components.begin(), connected_components.end(), [] (std::set<node_t> a, std::set<node_t> b) { return a.size() > b.size(); } );
     }
 
-    void construct_connectivity(LogicNetwork const& ntk) {
-        // topo_view rules out the nodes unreachable from pos
-        mockturtle::topo_view view{ntk};
-        view.foreach_node( [&] ( auto node ) {
+    void construct_connectivity() {
+        _view.foreach_node( [&] ( auto node ) {
             if (node_to_fanin.find(node) == node_to_fanin.end()) node_to_fanin[node] = std::set<node_t>();
-            ntk.foreach_fanin( node, [&] ( auto fanin ) {
+            if (node_to_fanout.find(node) == node_to_fanout.end()) node_to_fanout[node] = std::set<node_t>();
+            _view.foreach_fanin( node, [&] ( auto fanin ) {
                 node_to_fanin[node].insert(fanin.index);
                 if (node_to_fanout.find(fanin.index) == node_to_fanout.end()) node_to_fanout[fanin.index] = std::set<node_t>();
                 node_to_fanout[fanin.index].insert(node);
@@ -234,13 +317,13 @@ private:
     }
 
     inline void clear_containers() {
-        connected_components.clear();
         node_to_fanin.clear();
         node_to_fanout.clear();
         pos.clear();
     }
-    std::vector<std::set<node_t>> connected_components;
-    std::unordered_set<node_t> pos;
+    LogicNetwork _ntk;
+    LogicNetwork _view;
+    std::set<node_t> pos;
     std::unordered_map<node_t, std::set<node_t>> node_to_fanin;
     std::unordered_map<node_t, std::set<node_t>> node_to_fanout;
 };
