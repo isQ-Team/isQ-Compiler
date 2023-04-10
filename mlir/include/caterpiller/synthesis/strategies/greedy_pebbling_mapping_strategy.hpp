59667cf26408a5c37c193daa2d58aef84b69d69a
root <root@LAPTOP-JC95M2KP>
2023-04-10 16:02:49 +0800

    bug fix

--- mlir/include/caterpiller/synthesis/strategies/greedy_pebbling_mapping_strategy.hpp
+++ mlir/include/caterpiller/synthesis/strategies/greedy_pebbling_mapping_strategy.hpp
@@ -23,9 +23,6 @@ public:
         static_assert( mt::is_network_type_v<LogicNetwork>, "LogicNetwork is not a network type" );
         static_assert( mt::has_foreach_po_v<LogicNetwork>, "LogicNetwork does not implement the foreach_po method" );
         static_assert( mt::has_foreach_fanin_v<LogicNetwork>, "LogicNetwork does not implement the foreach_fanin method" );
-        static_assert( mt::has_foreach_node_v<LogicNetwork>, "LogicNetwork does not implement the foreach_node method" );
-        static_assert( mt::has_is_constant_v<LogicNetwork>, "LogicNetwork does not implement the is_constant method" );
-        static_assert( mt::has_is_pi_v<LogicNetwork>, "LogicNetwork does not implement the is_pi method" );
         static_assert( mt::has_set_visited_v<LogicNetwork>, "LogicNetwork does not implement the set_visited method" );
         static_assert( mt::has_visited_v<LogicNetwork>, "LogicNetwork does not implement the visited method" );
         static_assert( mt::has_clear_visited_v<LogicNetwork>, "LogicNetwork does not implement the clear_visited method" );
@@ -43,6 +40,10 @@ public:
         _view = view;
         construct_connectivity();
 
+        // validate target size
+        if (recursive_target_ratio) recursive_target_size = int(_view.size() * recursive_target_ratio);
+        if (recursive_target_size <= 5) recursive_target_size = 5;
+
         // set start, target and all nodes
         std::set<node_t> start;
         std::set<node_t> target;
@@ -50,21 +51,20 @@ public:
         view.foreach_node( [&] ( auto node ) {
             if (view.is_constant(node) || view.is_pi(node)) start.insert(node);
             nodes.insert(node);
+            hasbeen_computed[node] = false;
         } );
         ntk.foreach_po( [&] ( auto sig, auto po_index ) {
             auto po = ntk.get_node(sig);
-            target.insert(po);
-            pos.insert(po);
+            if (!start.count(po)) target.insert(po);
         } );
         greedy_pebble(nodes, start, target);
-        /*
-        compute_connected_components(ntk);
-        for (auto connected_component : connected_components) {
-            greedy_pebble(connected_component, ntk);
-        }*/
-        // TODO
         return true;
     }
+
+    // target size under which recursion stops
+    void set_target_size(int s) { recursive_target_size = s; }
+
+    void set_target_ratio(double r) { recursive_target_ratio = r; }
 
     void print_connected_component(std::ostream &os, std::vector<std::set<node_t>> connected_components) {
         os << "******connected component******" << std::endl;
@@ -138,58 +138,74 @@ private:
         return result;
     }
 
-    // target is a subset of s
-    void compute_set_steps(std::set<node_t>& s, std::set<node_t>& target, bool reverse = false) {
-        auto it = this->steps().end();
-        _view.foreach_node( [&] ( auto node ) {
-            if (_view.is_constant(node) || _view.is_pi(node)) return;
-            if (!s.count(node)) return;
-            if (target.count(node)) {
-                if (reverse) {
-                    if (pos.count(node)) return;
-                    it = this->steps().insert(it, {node, uncompute_action{}});
-                    it++;
-                } else {
-                    it = this->steps().insert(it, {node, compute_action{}});
-                    it++;
-                }
-            } else {
-                it = this->steps().insert(it, {node, compute_action{}});
-                it++;
-                it = this->steps().insert(it, {node, uncompute_action{}});
-            }
-        } );
-        /*
-        std::cout << "print steps: " << std::endl;
-        auto it_t = this->steps().begin();
-        while (it_t != this->steps().end()) {
-            std::cout << it_t->first << std::endl;
-            it_t++;
+    void compute_node(node_t node, bool compute = true) {
+        if (compute == hasbeen_computed[node]) return;
+        if (compute) {
+            this->steps().emplace_back(node, compute_action{});
+            hasbeen_computed[node] = true;
+        } else {
+            this->steps().emplace_back(node, uncompute_action{});
+            hasbeen_computed[node] = false;
         }
-        std::cout << "print steps ends here" << std::endl;*/
+    }
+    
+    void uncompute_eagerly(node_t node, const std::set<node_t>& s, const std::set<node_t>& target, std::map<node_t, int>& ref_count, bool reverse = false) {
+        if (_ntk.is_constant(node) || _ntk.is_pi(node)) return;
+        for (auto fanin : node_to_fanin[node]) {
+            if (!s.count(fanin)) continue;
+            --ref_count[fanin];
+            if (!ref_count[fanin]) {
+                if (!reverse && target.count(fanin)) continue;
+                compute_node(fanin, false);
+                uncompute_eagerly(fanin, s, target, ref_count, reverse);
+            }
+        }
     }
 
+    // use eager cleanup
+    // target is a subset of s
+    void compute_set_steps(std::set<node_t>& s, std::set<node_t>& target, bool reverse = false) {
+        std::map<node_t, int> ref_count;
+        _view.foreach_node( [&] ( auto node ) {
+            if (!s.count(node)) return;
+            ref_count[node] = intersection(node_to_fanout[node], s).size();
+        } );
+        _view.foreach_node( [&] ( auto node ) {
+            if (_ntk.is_constant(node) || _ntk.is_pi(node)) return;
+            if (!s.count(node)) return;
+            if (target.count(node)) {
+                if (reverse && ref_count[node]) return;
+                compute_node(node, !reverse);
+                uncompute_eagerly(node, s, target, ref_count, reverse);
+            } else {
+                compute_node(node);
+            }
+        } );
+    }
+    
+    // if reverse = false, begin with pebbling configuration P = start and end with P = start + target. 
+    // if reverse = true, begin with pebbling configuration P = start + target and end with P = start. 
     void greedy_pebble(std::set<node_t> nodes, std::set<node_t> start, std::set<node_t> target, bool reverse = false) {
         std::vector<std::set<node_t>> connected_components;
         compute_connected_components(nodes, connected_components);
 
         for (auto connected_component : connected_components) {
-            divide_and_conquer(connected_component, start, intersection(connected_component, target), reverse);
+            auto live = live_node(connected_component, target);
+            divide_and_conquer(live, start, intersection(live, target), reverse);
         }
     }
     
     // target <= nodes, intersection(nodes, start) = empty
     void divide_and_conquer(std::set<node_t> nodes, std::set<node_t> start, std::set<node_t> target, bool reverse = false) {
-        if (difference(nodes, target).size() <= 4) {
+        if (nodes.size() - target.size() <= recursive_target_size) {
             compute_set_steps(nodes, target, reverse);
             return;
         }
-        int beta = nodes.size() * 2 / 3;
 
         std::set<node_t> A;
         std::set<node_t> B = difference(nodes, A);  // V - A
         std::set<node_t> C = RN(start, nodes);
-        int target_size = nodes.size() - beta;
+        int target_size = (nodes.size() - target.size()) >> 1;
         while (A.size() <= target_size) {
             int rn_max_size = 0, n_max_size = 0;
             int rn_now_size = C.size();
@@ -213,31 +229,11 @@ private:
                     rn_max_size = rn_size_i;
                     n_max_size = n_size_i;
                 }
-                /*
-                std::set<node_t> i_A;  // {i} + A
-                std::copy(A.begin(), A.end(), std::inserter(i_A, i_A.begin()));
-                i_A.insert(i);
-                std::set<node_t> rn_i_A = RN(set_union(i_A, start), nodes);
-                std::set<node_t> n_i_B = intersection(B, node_to_fanout[i]);
-                
-                if (rn_i_A.size() > rn_max_size || (rn_i_A.size() == rn_max_size && n_i_B.size() > n_max_size)) {
-                    next = i;
-                    rn_max_size = rn_i_A.size();
-                    n_max_size = n_i_B.size();
-                }*/
-                /*
-                std::cout << i << std::endl;
-                std::cout << "rn_i_A.size: " << rn_i_A.size() << std::endl;
-                std::cout << "rn_max_size: " << rn_max_size << std::endl;
-                std::cout << "n_i_B.size: " << n_i_B.size() << std::endl;
-                std::cout << "n_max_size: " << n_max_size << std::endl;
-                */
-                
             }
             A.insert(next);
             B.erase(next);  // V - A
+
             // update C
-            
             C.erase(next);
             for (auto fanout : node_to_fanout[next]) {
                 if (!nodes.count(fanout)) continue;
@@ -249,8 +245,8 @@ private:
                 }
                 if (all_fanin_in_A) C.insert(fanout);
             }
-            //C = RN(set_union(A, start), nodes);
             /*
+            std::cout << next << " added to A" << std::endl;
             print_to(std::cout, A, "A");
             print_to(std::cout, B, "B");
             print_to(std::cout, C, "C");
@@ -258,8 +254,9 @@ private:
         }
         std::set<node_t> fanout_of_start = RN(start, nodes);
         C = difference(C, fanout_of_start);
-        //C = RN(A, nodes);
-        std::set<node_t> S = RP(difference(B, C), nodes);
+
+        std::set<node_t> target_C = intersection(target, C);
+        std::set<node_t> S = set_union(RP(difference(B, C), nodes), target_C);
         A = difference(A, S);
         B = difference(B, S);
         /*
@@ -268,9 +265,44 @@ private:
         print_to(std::cout, B, "B");
         print_to(std::cout, S, "S");
         */
-        greedy_pebble(set_union(A, S), start, set_union(S, intersection(target, A)));
-        greedy_pebble(B, set_union(S, start), intersection(target, B), reverse);
-        greedy_pebble(set_union(A, S), start, set_union(S, intersection(target, A)), true);
+        std::set<node_t> target_A = intersection(target, A);
+        std::set<node_t> target_B = intersection(target, B);
+        std::set<node_t> target_S = intersection(target, S);
+        std::set<node_t> none_target_A = difference(A, target_A);
+        std::set<node_t> none_target_S = difference(S, target_S);
+        if (reverse) {
+            greedy_pebble(set_union(none_target_A, none_target_S), set_union(set_union(target_A, start), target_S), difference(S, target_S));
+            greedy_pebble(B, set_union(S, start), target_B, true);
+            greedy_pebble(set_union(A, S), start, set_union(S, target_A), true);
+        } else {
+            greedy_pebble(set_union(A, S), start, set_union(S, target_A));
+            greedy_pebble(B, set_union(S, start), target_B);
+            greedy_pebble(set_union(none_target_A, none_target_S), set_union(set_union(target_A, start), target_S), difference(S, target_S), true);
+        }
+    }
+
+    // nodes that do lead to a target
+    std::set<node_t> live_node(std::set<node_t>& nodes, std::set<node_t>& target) {
+        std::set<node_t> alive;
+        std::stack<node_t> sta;
+        for (auto t : target) {
+            if (nodes.count(t)) sta.push(t);
+        }
+        _ntk.clear_visited();
+        while (!sta.empty()) {
+            auto node = sta.top();
+            sta.pop();
+            if (!nodes.count(node)) continue;
+            if (_ntk.is_constant(node) || _ntk.is_pi(node)) continue;
+            if (_ntk.visited(node)) continue;
+            alive.insert(node);
+            _ntk.set_visited(node, 1);
+            for (auto fanin : node_to_fanin[node]) {
+                if (!_ntk.visited(fanin)) sta.push(fanin);
+            }
+        }
+        _ntk.clear_visited();
+        return alive;
     }
 
     void compute_connected_components(std::set<node_t>& nodes, std::vector<std::set<node_t>>& connected_components) 
@@ -319,13 +351,15 @@ private:
     inline void clear_containers() {
         node_to_fanin.clear();
         node_to_fanout.clear();
-        pos.clear();
+        hasbeen_computed.clear();
     }
     LogicNetwork _ntk;
     LogicNetwork _view;
-    std::set<node_t> pos;
+    std::unordered_map<node_t, bool> hasbeen_computed;
     std::unordered_map<node_t, std::set<node_t>> node_to_fanin;
     std::unordered_map<node_t, std::set<node_t>> node_to_fanout;
+    int recursive_target_size = 5;
+    double recursive_target_ratio = 0.0;
 };
 
 } // namespace caterpillar
