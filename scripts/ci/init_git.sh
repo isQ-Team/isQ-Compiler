3fe115b7c7809b339c2f3c96200de9f2e3d40eaf
gjz010 <gjz010944@gmail.com>
2023-07-03 16:39:28 +0800

    wip: fix ci

--- scripts/ci/init_git.sh
+++ scripts/ci/init_git.sh
@@ -3,3 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 git config --global user.email "gitlab-ci@arclighttest.cn"
 git config --global user.name "Gitlab Runner"
+git remote add origin-push ${CI_SERVER_PROTOCOL}://gitlab-ci:${	
+GITLAB_CI_PUSH_TOKEN}@${CI_SERVER_HOST}:${CI_SERVER_PORT}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}.git
